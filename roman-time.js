fe74e72d07ac61caeaaf2400e416b237ef1ab643
Khapov-Kirill <kulc_hacker@mail.ru>
2015-10-05 20:33:35 +0400

    Нашел алгоритм
    К сожалению, придумал не я :'(

--- roman-time.js
+++ roman-time.js
@@ -1,44 +1,25 @@
-function small(num){
-	switch (num){
-		case 0: break;
-		case 1: str += "X"; break;
-		case 2: str += "XX"; break;
-		case 3: str += "XXX"; break;
-		case 4: str += "XL"; break;
-		case 5: str += "L"; break;
-	}
+function translate(n){
+	for (var i = a.length - 1; i >= 0; i--) {
+        while (n >= b[i]) {
+            str += a[i];
+            n -= b[i];
+        }
+    }
 }
-function big(num){
-	switch (num){
-		case 0: break;
-		case 1: str += "I"; break;
-		case 2: str += "II"; break;
-		case 3: str += "III"; break;
-		case 4: str += "IV"; break;
-		case 5: str += "V"; break;
-		case 6: str += "VI"; break;
-		case 7: str += "VII"; break;
-		case 8: str += "VIII"; break;
-		case 9: str += "IX"; break;
-	}
-	return;
-}
-// Begin
+
 var hours = process.argv[2];
 var minutes = process.argv[3];
 
-var h_dec = Math.floor(hours/10);
-var h_uni = hours%10;
-var m_dec = Math.floor(minutes/10);
-var m_uni = minutes%10;
+var a = new Array("I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M");
+var b = new Array(1,4,5,9,10,40,50,90,100,400,500,900,1000);
 var str = "";
-if((hours > 23 || hours < 0) || (minutes > 59 || minutes < 0))
-	console.log("Data is incorrect");
+
+if(hours > 23 || hours < 0 || minutes > 59 || minutes < 0)
+	console.log("Incorrect data");
 else {
-	small(h_dec);
-	big(h_uni);
+	translate(hours);
 	str += ":";
-	small(m_dec);
-	big(m_uni);
+	translate(minutes);
 	console.log(str);
 }
+
